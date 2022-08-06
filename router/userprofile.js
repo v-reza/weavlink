79e15081a4fa0f60ac800be18579b1c8477bfb51
v-reza <mrezalf0@gmail.com>
2022-08-06 16:03:57 +0700

    feat crud skills

--- router/userprofile.js
+++ router/userprofile.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const verifyBearerToken = require("../helper/verifyBearerToken");
 const User = require("../models/User");
 const UserProfile = require("../models/UserProfile");
+const UserSkills = require("../models/UserSkills");
 
 router.get("/:id", async (req, res) => {
   try {
@@ -38,6 +39,43 @@ router.put("/", verifyBearerToken, async (req, res) => {
 
     const userUpdated = await User.findById(req.user.id);
     res.status(200).json(userUpdated);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get("/render/skills", verifyBearerToken, async (req, res) => {
+  try {
+    const userSkills = await UserSkills.findOne({ userId: req.user.id });
+    res.status(200).json(userSkills);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.post("/add/skills", verifyBearerToken, async (req, res) => {
+  try {
+    const userSkills = await UserSkills.findOne({ userId: req.user.id });
+    await userSkills.updateOne({
+      $push: {
+        skills: req.body.skills,
+      },
+    });
+    return res.status(200).json("Skill has been added");
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.delete("/delete/skills/:skillname", verifyBearerToken, async (req, res) => {
+  try {
+    const userSkills = await UserSkills.findOne({ userId: req.user.id });
+    await userSkills.updateOne({
+      $pull: {
+        skills: req.params.skillname,
+      },
+    });
+    return res.status(200).json("Skill has been deleted");
   } catch (error) {
     return res.status(500).json(error);
   }
