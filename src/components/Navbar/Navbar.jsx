e32fc075b9250a1969b9adc4150f75498872f32e
v-reza <mrezalf0@gmail.com>
2022-08-07 01:51:29 +0700

    dark layout mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -111,14 +111,14 @@ const Navbar = () => {
                   <div className="max-w-lg w-full lg:max-w-xs">
                     <button
                       type="button"
-                      class="hidden w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-300 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700"
+                      className="hidden w-full lg:flex items-center text-sm leading-6 text-slate-400 rounded-md ring-1 ring-slate-900/10 shadow-sm py-1.5 pl-2 pr-3 hover:ring-slate-300 dark:bg-slate-800 dark:highlight-white/5 dark:hover:bg-slate-700"
                     >
                       <svg
                         width="24"
                         height="24"
                         fill="none"
                         aria-hidden="true"
-                        class="mr-3 flex-none"
+                        className="mr-3 flex-none"
                       >
                         <path
                           d="m19 19-3.5-3.5"
@@ -138,7 +138,7 @@ const Navbar = () => {
                         ></circle>
                       </svg>
                       Quick search...
-                      <span class="ml-auto pl-3 flex-none text-xs font-semibold">
+                      <span className="ml-auto pl-3 flex-none text-xs font-semibold">
                         Ctrl K
                       </span>
                     </button>
@@ -213,7 +213,7 @@ const Navbar = () => {
                       <circle cx="11" cy="11" r="6"></circle>
                     </svg>
                   </button>
-                  <Disclosure.Button className="bg-gray-50 p-2 inline-flex items-center justify-center rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500">
+                  <Disclosure.Button className="bg-gray-50 dark:bg-slate-900 p-2 inline-flex items-center justify-center rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -228,7 +228,7 @@ const Navbar = () => {
                   <div className="flex items-center">
                     <button
                       type="button"
-                      className="mr-2 bg-gray-50 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
+                      className="mr-2 bg-gray-50 dark:bg-slate-900 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
                     >
                       <span className="sr-only">Theme Mode</span>
                       {theme === "dark" ? (
@@ -251,7 +251,7 @@ const Navbar = () => {
                     </button>
                     <button
                       type="button"
-                      className="bg-gray-50 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
+                      className="bg-gray-50 dark:bg-slate-900 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
                     >
                       <span className="sr-only">View notifications</span>
                       <BellIcon className="h-6 w-6" aria-hidden="true" />
@@ -260,7 +260,7 @@ const Navbar = () => {
                     {/* Profile dropdown */}
                     <Menu as="div" className="ml-3 relative flex-shrink-0">
                       <div>
-                        <Menu.Button className="bg-gray-50 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500">
+                        <Menu.Button className="bg-gray-50 dark:bg-slate-900 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500">
                           <span className="sr-only">Open user menu</span>
                           <img
                             className="rounded-full h-8 w-8"
@@ -303,7 +303,7 @@ const Navbar = () => {
               </div>
             </div>
 
-            <Disclosure.Panel className="bg-gray-50 border-b border-gray-200 lg:hidden">
+            <Disclosure.Panel className="bg-gray-50 dark:bg-slate-900/95 border-b border-gray-200 lg:hidden">
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item) => (
                   <Disclosure.Button
@@ -311,8 +311,8 @@ const Navbar = () => {
                     as="a"
                     href={item.href}
                     className={classNames(
-                      item.current ? "bg-gray-100" : "hover:bg-gray-100",
-                      "block px-3 py-2 rounded-md font-medium text-gray-900"
+                      item.current ? "bg-gray-100 dark:bg-slate-500 dark:text-white" : "hover:bg-gray-100 dark:hover:bg-slate-500",
+                      "block px-3 py-2 rounded-md font-medium text-gray-900 dark:text-white"
                     )}
                     aria-current={item.current ? "page" : undefined}
                   >
@@ -331,24 +331,24 @@ const Navbar = () => {
                     />
                   </div>
                   <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">
+                    <div className="text-base font-medium text-gray-800 dark:text-white">
                       {user.firstname} {user.lastname}
                     </div>
-                    <div className="text-sm font-medium text-gray-500">
+                    <div className="text-sm font-medium text-gray-500 dark:text-slate-300">
                       {user.email}
                     </div>
                   </div>
 
                   <button
                     type="button"
-                    className="mr-auto bg-gray-50 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
+                    className="mr-auto bg-gray-50 dark:bg-slate-900 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
                   >
                     <span className="sr-only">View notifications</span>
                     <BellIcon className="h-6 w-6" aria-hidden="true" />
                   </button>
                   <button
                     type="button"
-                    className="ml-auto bg-gray-50 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
+                    className="ml-auto bg-gray-50 dark:bg-slate-900 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-rose-500"
                   >
                     <span className="sr-only">Theme Mode</span>
                     {theme === "dark" ? (
@@ -377,7 +377,7 @@ const Navbar = () => {
                       as="a"
                       href={item.href}
                       onClick={item.onclick}
-                      className="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-100"
+                      className="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-100 dark:text-white "
                     >
                       {item.name}
                     </Disclosure.Button>
