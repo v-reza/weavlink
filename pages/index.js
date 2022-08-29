070c0c1c9ebb4cd5d61128ca393893cbd2fd41d6
v-reza <mrezalf0@gmail.com>
2022-08-29 17:30:03 +0700

    custom padding card

--- pages/index.js
+++ pages/index.js
@@ -141,13 +141,13 @@ export default function Home() {
             </Sidebar>
           </Container.Sidebar>
           <Container.Main>
-            <Card>
+            <Card padding={"p-4"}>
               <div className="flex items-center space-x-4">
                 <img
                   src={
                     user?.profilePicture ? user.profilePicture : "/avatar.png"
                   }
-                  className="w-10 h-10 rounded-full object-cover"
+                  className="ml-2 mt-1 w-10 h-10 rounded-full object-cover"
                   alt=""
                 />
                 <Button
@@ -160,33 +160,43 @@ export default function Home() {
                   justify="start"
                   rounded="full"
                 >
-                  <span className="text-white text-base">Start a post</span>
+                  <span className="text-white text-sm font-medium">
+                    Start a post
+                  </span>
                 </Button>
               </div>
               <div className="flex items-center justify-between space-x-4 mt-6">
-                <div className="flex items-center cursor-pointer">
-                  <PhotographIcon className="h-6 w-6 text-blue-500" />
-                  <span className="text-white ml-2 text-sm font-medium">
-                    Photo
-                  </span>
+                <div className="cursor-pointer hover:bg-slate-500/30 px-4 py-1.5 rounded-lg">
+                  <div className="flex items-center cursor-pointer">
+                    <PhotographIcon className="h-6 w-6 text-blue-500" />
+                    <span className="text-white ml-2 text-sm font-medium">
+                      Photo
+                    </span>
+                  </div>
                 </div>
-                <div className="flex items-center cursor-pointer">
-                  <PlayIcon className="h-6 w-6 text-green-500" />
-                  <span className="text-white ml-2 text-sm font-medium">
-                    Video
-                  </span>
+                <div className="cursor-pointer hover:bg-slate-500/30 px-4 py-1 rounded-lg">
+                  <div className="flex items-center cursor-pointer">
+                    <PlayIcon className="h-6 w-6 text-green-500" />
+                    <span className="text-white ml-2 text-sm font-medium">
+                      Video
+                    </span>
+                  </div>
                 </div>
-                <div className="flex items-center cursor-pointer">
-                  <CalendarIcon className="h-6 w-6 text-amber-500" />
-                  <span className="text-white ml-2 text-sm font-medium">
-                    Event
-                  </span>
+                <div className="cursor-pointer hover:bg-slate-500/30 px-4 py-1 rounded-lg">
+                  <div className="flex items-center cursor-pointer">
+                    <CalendarIcon className="h-6 w-6 text-amber-500" />
+                    <span className="text-white ml-2 text-sm font-medium">
+                      Event
+                    </span>
+                  </div>
                 </div>
-                <div className="flex items-center cursor-pointer">
-                  <ClipboardListIcon className="h-6 w-6 text-pink-500" />
-                  <span className="text-white ml-2 text-sm font-medium">
-                    Write article
-                  </span>
+                <div className="cursor-pointer hover:bg-slate-500/30 px-4 py-1 rounded-lg">
+                  <div className="flex items-center cursor-pointer">
+                    <ClipboardListIcon className="h-6 w-6 text-pink-500" />
+                    <span className="text-white ml-2 text-sm font-medium">
+                      Write article
+                    </span>
+                  </div>
                 </div>
               </div>
             </Card>
--- src/uiComponents/Card.js
+++ src/uiComponents/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = ({ children, ...props }) => {
+const Card = ({ children, padding, ...props }) => {
   return (
     <div {...props}>
       <ul role="list" className="space-y-4">
         <div className="overflow-hidden sm:rounded-md shadow-slate-800">
           <div className="rounded-lg shadow bg-slate-800">
-            <div className="p-8">{children}</div>
+            <div className={`${padding || `p-8`}`}>{children}</div>
           </div>
         </div>
       </ul>
