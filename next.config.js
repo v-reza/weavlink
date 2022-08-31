32c9161c1479014214a8f49c5717e55fdf6dec18
v-reza <mrezalf0@gmail.com>
2022-08-31 12:07:00 +0700

    next config prod

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    // NEXT_APP_API: "https://velkeyapi.herokuapp.com/api",
-    // NEXT_APP_API_IMAGES : "https://velkeyapi.herokuapp.com/images/"
+    NEXT_APP_API: "https://velkeyapi.herokuapp.com/api",
+    NEXT_APP_API_IMAGES : "https://velkeyapi.herokuapp.com/images/"
   }
 };
 
