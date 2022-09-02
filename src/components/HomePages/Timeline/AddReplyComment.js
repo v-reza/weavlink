dbab153121a651fd02440cf4e22682b42c95e247
v-reza <mrezalf0@gmail.com>
2022-09-02 17:52:25 +0700

    change flow mentions

--- src/components/HomePages/Timeline/AddReplyComment.js
+++ src/components/HomePages/Timeline/AddReplyComment.js
@@ -3,7 +3,6 @@ import useAuth from "@/hooks/useAuth";
 import useGlobal from "@/hooks/useGlobal";
 import useHeader from "@/hooks/useHeader";
 import useLoading from "@/hooks/useLoading";
-import InputMentions from "@/uiComponents/inputMentions";
 import { axiosGet, axiosPost } from "@/utils/axiosInstance";
 import {
   CheckIcon,
@@ -28,6 +27,9 @@ const AddReplyComment = ({ comment }) => {
   const [messageReply, setMessageReply] = useState("");
   const [openMentionsPeople, setOpenMentionsPeople] = useState(false);
   const [listUser, setListUser] = useState([]);
+  const [mentionsPeople, setMentionsPeople] = useState("");
+  const [listMentionsPeople, setListMentionsPeople] = useState([]);
+  console.log(listMentionsPeople);
   /* End State */
 
   /* Action */
@@ -65,29 +67,27 @@ const AddReplyComment = ({ comment }) => {
   };
 
   const filteredListUser =
-    messageReply === ""
+    mentionsPeople === ""
       ? []
       : listUser.filter((user) => {
           return (
             user.firstname
               .toLowerCase()
-              .includes(messageReply.replace(/[@]/u, "").toLowerCase()) ||
+              .includes(mentionsPeople.replace(/[@]/u, "").toLowerCase()) ||
             user.lastname
               .toLowerCase()
-              .includes(messageReply.replace(/[@]/u, "").toLowerCase())
+              .includes(mentionsPeople.replace(/[@]/u, "").toLowerCase())
           );
         });
-
   /* End Action */
 
   /* useEffect */
-  useEffect(() => {
-    if (/[@]/u.test(messageReply)) {
-      setOpenMentionsPeople(true);
-    } else {
-      setOpenMentionsPeople(false);
-    }
-  }, [messageReply]);
+
+  // useEffect(() => {
+  //   // if (/[@]/u.test(messageReply)) {
+  //     // setMentionsPeople(messageReply.replaceAll(messageReply, ""));
+  //   // }
+  // }, [messageReply]);
 
   useEffect(() => {
     const getListUser = async () => {
@@ -98,20 +98,47 @@ const AddReplyComment = ({ comment }) => {
   }, [user?._id]);
 
   /* End useEffect */
-  console.log(filteredListUser);
-  // console.log(messageReply.replace(/[@]/u, ""));
-
   return (
-    <div className="ml-6 p-2">
+    <div className="ml-4 p-2">
       <label htmlFor="reply" className="sr-only">
         Add your reply
       </label>
       <div className="flex">
+        <div
+          onClick={() => {
+            setOpenMentionsPeople(!openMentionsPeople);
+          }}
+          className="flex items-center justify-center text-md font-medium text-slate-400 cursor-pointer px-2 py-1 hover:bg-slate-700/50 mr-2  rounded-md"
+        >
+          @
+        </div>
         <div className="relative rounded-md shadow-sm w-full">
           <input
             ref={replyRef}
             value={messageReply}
-            onChange={(e) => setMessageReply(e.target.value)}
+            onChange={(e) => {
+              setMessageReply(e.target.value);
+              // if (messageReply.length > 0) {
+              // setMessageReply(messageReply.replace(/[@]/u, ""));
+              // setMentionsPeople(
+              //   messageReply.replaceAll(
+              //     messageReply,
+              //     e.target.value.replaceAll(
+              //       messageReply,
+              //       mentionsPeople.replace(messageReply, "")
+              //     )
+              //   )
+              // );
+              // } else {
+              //   setMentionsPeople(
+              //     messageReply.replace(messageReply, e.target.value)
+              //   );
+              // }
+              // if (/[@]/u.test(messageReply)) {
+              // } else {
+              //   setMentionsPeople(messageReply.replaceAll("@", ""));
+              // }
+            }}
             onKeyPress={(e) => handleNewReply(e)}
             type="text"
             className="bg-transparent text-slate-300 block w-full text-xs pr-10 sm:text-sm border-2 border-slate-600 rounded-full focus:outline-0 focus:border-slate-500 focus:ring-0"
@@ -125,20 +152,41 @@ const AddReplyComment = ({ comment }) => {
           </div>
         </div>
       </div>
-      {filteredListUser.length > 0 && (
+      {openMentionsPeople && (
         <div
           className={classNames(
             openMentionsPeople ? "block" : "hidden",
             "px-4 mt-1 relative"
           )}
         >
-          <div className="z-20 absolute w-full rounded-sm bg-slate-700 border border-slate-600 overflow-y-scroll h-32 ...">
+          <div className="z-20 absolute w-full rounded-md bg-slate-700 border border-slate-600 overflow-y-scroll h-56 ...">
             <div className="px-4 py-2">
-              {filteredListUser.length > 0 &&
+              <input
+                type="text"
+                value={mentionsPeople}
+                onChange={(e) => setMentionsPeople(e.target.value)}
+                className="mb-4 bg-slate-800 text-slate-300 block w-full text-xs pr-10 sm:text-sm border-2 border-slate-700 rounded-full focus:outline-0 focus:border-slate-600 focus:ring-0"
+                placeholder="Search..."
+              />
+              {filteredListUser.length > 0 ? (
                 filteredListUser.map((user) => (
                   <div
                     className="flex space-x-3 mb-4 cursor-pointer"
                     key={user._id}
+                    onClick={() => {
+                      setMentionsPeople("");
+                      const cursor = replyRef.current.selectionStart;
+                      const text =
+                        messageReply.slice(0, cursor) +
+                        `@${user.firstname} ${user.lastname} ` +
+                        messageReply.slice(cursor);
+                      setMessageReply(messageReply.replace(messageReply, text));
+                      setListMentionsPeople([
+                        ...listMentionsPeople,
+                        user.firstname + " " + user.lastname,
+                      ]);
+                      setOpenMentionsPeople(false);
+                    }}
                   >
                     <div className="flex-shrink-0">
                       <img
@@ -163,7 +211,12 @@ const AddReplyComment = ({ comment }) => {
                       </div>
                     </div>
                   </div>
-                ))}
+                ))
+              ) : (
+                <span className="text-slate-300 font-medium flex items-center justify-center">
+                  No result...
+                </span>
+              )}
             </div>
           </div>
         </div>
