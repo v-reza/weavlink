7d1a666205b6840be6c0ca44136745b2b7be5426
v-reza <mrezalf0@gmail.com>
2022-09-03 16:25:16 +0700

    next config

--- next.config.js
+++ next.config.js
@@ -6,26 +6,6 @@ const nextConfig = {
     NEXT_APP_API: "https://api.weavlink.works/api",
     NEXT_APP_API_IMAGES: "https://api.weavlink.works/images/",
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 const withPWA = require("next-pwa")({
