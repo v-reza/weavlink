4a883a1a47638fd8403db0452b91a331d6ad40a2
v-reza <mrezalf0@gmail.com>
2022-09-03 16:05:46 +0700

    prod next config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // env: {
-  //   NEXT_APP_API: "https://velkeyapi.herokuapp.com/api",
-  //   NEXT_APP_API_IMAGES : "https://velkeyapi.herokuapp.com/images/"
-  // }
+  env: {
+    NEXT_APP_API: "https://api.weavlink.works/api",
+    NEXT_APP_API_IMAGES : "https://api.weavlink.works/images/"
+  }
 };
 
 const withPWA = require("next-pwa")({
