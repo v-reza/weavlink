9fd14df5ec481bc5f84eddb024671c838f3fb9b5
v-reza <mrezalf0@gmail.com>
2022-09-03 16:13:54 +0700

    cors client

--- next.config.js
+++ next.config.js
@@ -4,8 +4,28 @@ const nextConfig = {
   swcMinify: true,
   env: {
     NEXT_APP_API: "https://api.weavlink.works/api",
-    NEXT_APP_API_IMAGES : "https://api.weavlink.works/images/"
-  }
+    NEXT_APP_API_IMAGES: "https://api.weavlink.works/images/",
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withPWA = require("next-pwa")({
