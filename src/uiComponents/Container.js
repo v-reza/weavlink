62cdf2b77cfed2ae2a335b545e963eddd7fead62
v-reza <mrezalf0@gmail.com>
2022-09-07 20:45:05 +0700

    container

--- src/uiComponents/Container.js
+++ src/uiComponents/Container.js
@@ -1,32 +1,34 @@
 /* eslint-disable react/display-name */
 import React from "react";
 
-const Container = ({ children, ...props }) => (
-  <div className="min-h-full">
-    <div className="py-10">
-      <div className="max-w-3xl mx-auto sm:px-6 xs:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
-        {children}
+const Container = ({ children, ...props }) => {
+  return (
+    <div className="min-h-full">
+      <div className="py-10">
+        <div className="max-w-3xl mx-auto sm:px-6 xs:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
+          {children}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Container.Sidebar = ({ children, ...props }) => <>{children}</>;
 
 Container.Main = ({ lg = "9", xl = "6", children }) => {
   return (
-    <main className={`lg:col-span-${lg || 9} xl:col-span-${xl || 6}`}>
-      {children}
-    </main>
+    <main className={`lg:col-span-${lg} xl:col-span-${xl}`}>{children}</main>
   );
 };
 
-Container.Rightbar = ({ children, sm, md, lg = "2", xl = "2", ...props }) => (
-  <aside
-    className={`hidden xl:block xl:col-span-${xl} lg:block lg:col-span-${lg}`}
-  >
-    <div className="top-4 space-y-4">{children}</div>
-  </aside>
-);
+Container.Rightbar = ({ children, sm, md, lg = "2", xl = "2", ...props }) => {
+  return (
+    <aside
+      className={`hidden xl:block xl:col-span-${xl} lg:block lg:col-span-${lg}`}
+    >
+      <div className="top-4 space-y-4">{children}</div>
+    </aside>
+  );
+};
 
 export default Container;
